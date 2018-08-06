83c8ca06dc549aa0a3269afce94b9854d1239cba
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-06 23:23:16 +0000

    implemented getRepoContributors, added url and headers properties

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,6 @@
-var httpFunctions = require('./http-functions');
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 var requestOptions = {
   host: 'sytantris.github.io',
@@ -9,4 +11,20 @@ function printHTML (html) {
   console.log(html);
 }
 
-httpFunctions.getHTML(requestOptions, printHTML);
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
