171779e27e09cc1fdabad5f22fea9d1f5b99632c
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-07 01:42:01 +0000

    added comments and fixed readibility

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,26 @@
+// require request package and secrets
 var request = require('request');
 var secrets = require('./secrets');
+// store owner and repo arguments
 var owner = process.argv[2];
 var repo = process.argv[3];
 
+// if the user forgets to enter in two arguments, let them know
 if (!owner || !repo) {
   console.log("This application require two arguments.  Please enter a username and repository");
   return;
 }
 
+// intro message to let them know the program is running
 console.log('Welcome to the GitHub Avatar Downloader!');
 console.log("Downloading Avatars...");
 
+/******************************************************* 
+* getRepoContributors: takes the username, repository and
+* callback function as arguments. Gets the http request
+* for the user-provided arguments and parses returned
+* body to JSON 
+*******************************************************/
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -22,10 +32,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
     var body = JSON.parse(body);
+    /******************************************************* 
+    * callback: takes the error, and JSON body as arguments.
+    * Loops through each collaborator's info and download's 
+    * their image
+    *******************************************************/
     cb(err, body);
   });
 }
 
+/******************************************************* 
+* downloadImageByURL: takes the username, repository and
+* callback function as arguments. Loops through each 
+* collaborator's info and download's their image
+*******************************************************/
+function downloadImageByURL(url, filePath) {
+  var fs = require('fs');
+  request.get(url)
+  .on('error', function (err) {
+    throw err; 
+  })
+  .pipe(fs.createWriteStream(filePath))
+}
+
+// run the program
 getRepoContributors(owner, repo, function(err, result) {
   if (err) {
     console.log("Errors:", err);
@@ -39,12 +69,3 @@ getRepoContributors(owner, repo, function(err, result) {
   }
   console.log("Successfully downloaded the avatars!");
 });
-
-function downloadImageByURL(url, filePath) {
-  var fs = require('fs');
-  request.get(url)
-  .on('error', function (err) {
-    throw err; 
-  })
-  .pipe(fs.createWriteStream(filePath))
-}
