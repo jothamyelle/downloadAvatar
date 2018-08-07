dd67df2cde796f992b2f2177074c62fe4811fead
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-07 00:17:41 +0000

    prints out contributors avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secrets = require('./secrets');
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -18,11 +20,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
+    var body = JSON.parse(body);
     cb(err, body);
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors(owner, repo, function(err, result) {
+  if (err) {
+    console.log("Errors:", err);
+    return;
+  }
+
+  for (element in result) {
+    console.log(result[element].avatar_url);
+  }
+
+  console.log("Yay!");
 });
